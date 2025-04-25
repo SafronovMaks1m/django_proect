d19de46de09057d7d04a1fce46f75de45f7c683b
SafronovMaks1m <safranov01@list.ru>
2025-04-25 21:20:08 +0300

    writing tests

--- Course_FirstProject/settings.py
+++ Course_FirstProject/settings.py
@@ -100,7 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
+# Inqernationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
@@ -121,3 +121,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,9 @@
 from django.shortcuts import render
-  
-def index(request):
-    return render(request, 'blog/index.html', context = {'body': '<h1>Hello World!</h1>'})
 
-from django.template.response import TemplateResponse
 
 def index(request):
-    return TemplateResponse(request, 'index.html')
+    return render(request, 'blog/index.html', context={'site': 'Stepik.org'})
 
-from django.shortcuts import render
 
-def profile(request):
-    data = request.GET.dict()
-    return render(request, 'profile.html', context = data)
+def about(request):
+    return render(request, 'blog/about.html', context={'site': 'Stepik'})
